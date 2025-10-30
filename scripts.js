4179c664c0f15bdea89484638f0e4ee99703d0f8
ProudMay <mehdi79nekouei@gmail.com>
2025-10-31 00:37:01 +0330

    feat [constructor  for books]

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const bookList=[];
+
+function Book(title,author,genre,pages,read){
+    this.title=title;
+    this.author=author;
+    this.genre=genre;
+    this.pages=pages;
+    this.read=read;
+}
