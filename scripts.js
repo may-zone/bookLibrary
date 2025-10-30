9528e364e54e77928c61e3edaa6b762e8d695a8c
ProudMay <mehdi79nekouei@gmail.com>
2025-10-31 00:49:47 +0330

    feat [add 3 sample book to bookList]

--- scripts.js
+++ scripts.js
@@ -13,3 +13,6 @@ function addBookToLibrary(title,author,genre,pages,read){
     const newBook = new Book(title,author,genre,pages,read);
     bookList.push(newBook);
 }
+addBookToLibrary("1984", "George Orwell", "Dystopian", 328, true);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "Fantasy", 310, false);
+addBookToLibrary("یک عاشقانه آرام", "نادر ابراهیمی", "عاشقانه", 237 ,false);
