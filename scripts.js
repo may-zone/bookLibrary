ea9dd60d9d28ccf98df685e428389ad2448f7782
ProudMay <mehdi79nekouei@gmail.com>
2025-10-31 01:01:15 +0330

    feat [render function]

--- scripts.js
+++ scripts.js
@@ -12,7 +12,25 @@ function Book(title,author,genre,pages,read = false){
 function addBookToLibrary(title,author,genre,pages,read){
     const newBook = new Book(title,author,genre,pages,read);
     bookList.push(newBook);
+    render();
 }
 addBookToLibrary("1984", "George Orwell", "Dystopian", 328, true);
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "Fantasy", 310, false);
 addBookToLibrary("یک عاشقانه آرام", "نادر ابراهیمی", "عاشقانه", 237 ,false);
+
+function render(){
+    const container = document.querySelector('.booth');
+    bookList.forEach(Book =>{
+        const card = document.createElement('.div');
+        card.classList.add('card');
+        card.innerHTML =`
+              <h3>${book.title}</h3>
+      <p><strong>Author:</strong> ${book.author}</p>
+      <p><strong>Genre:</strong> ${book.genre}</p>
+      <p><strong>Pages:</strong> ${book.pages}</p>
+      <p><strong>Read:</strong> ${book.read ? '✅ Yes' : '❌ No'}</p>
+      <button class="remove-btn" data-id="${book.id}">Remove</button>`;
+      container.appendChild(card);
+
+    });
+}
